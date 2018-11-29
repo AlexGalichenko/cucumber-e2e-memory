a43a22a7f9d38d79b5b1e384ac4d953fe8b5ad9b
Oleksandr Halichenko <Oleksandr.Halichenko@wolterskluwer.com>
2018-11-29 13:55:30 +0300

    * added test to check calculable with several params

--- test/calculable.test.js
+++ test/calculable.test.js
@@ -14,17 +14,26 @@ test("get calculable", () => {
     expect(Memory.parseValue("#FUNCTION")).toBe("value");
 });
 
-test("get calculable with arguments", () => {
+test("get calculable with 1 arguments", () => {
     class CalculablesMap extends AbstractCalculablesMap {
         constructor() {
             super();
-            this.defineCalculable(/GET_SAME_VALUE\(.+\)/, () => {
-                return "value"
-            });
+            this.defineCalculable(/GET_SAME_VALUE\(.+\)/, (a) => a);
         }
     }
     Memory.setCalculablesInstance(new CalculablesMap());
     expect(Memory.parseValue("#GET_SAME_VALUE(value)")).toBe("value");
+});
+
+test("get calculable with 2 arguments", () => {
+    class CalculablesMap extends AbstractCalculablesMap {
+        constructor() {
+            super();
+            this.defineCalculable(/CONCAT\(.+\)/, (a, b) => a + b);
+        }
+    }
+    Memory.setCalculablesInstance(new CalculablesMap());
+    expect(Memory.parseValue("#CONCAT(str, ing)")).toBe("string");
 });
 
 test("get not defined calculable", () => {
